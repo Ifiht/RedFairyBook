4f5c485cecbb4514d4896b77b790aba559253ddc
Peter Pan <peter@never.lan>
2024-05-02 07:42:24 -0400

    add tests

--- lib/redfairy.rb
+++ lib/redfairy.rb
@@ -3,18 +3,40 @@ require "yaml"
 class RedFairy
   def initialize(book)
     @book = book
+    @path = "#{Dir.home}/.config/#{@book}/config.yml"
+    if not Dir.exists?("#{Dir.home}/.config")
+      Dir.mkdir("#{Dir.home}/.config")
+      Dir.mkdir("#{Dir.home}/.config/#{@book}")
+      File.new @path
+    elsif not Dir.exists?("#{Dir.home}/.config/#{@book}")
+      Dir.mkdir("#{Dir.home}/.config/#{@book}")
+      File.new @path
+    elsif not File.exists?(@path)
+      File.new @path
+    end #if
     begin
-      @@config_hash = YAML.load(File.read("#{Dir.home}/.config/shikigami/config.yml"))
+      @config_hash = YAML.load(File.read(@path))
     rescue
-      @@config_hash = {}
-    end
+      @config_hash = {}
+      File.open(@path, 'w') { |f| f.write(YAML.dump(@config_hash)) }
+    end #begin
   end #def
 
   def self.exists?(key)
-    return @@config_hash.has_key?(key)
+    return @config_hash.has_key?(key)
   end #def
 
-  def get(key)
-    return @@config_hash[key]
+  def self.get(key)
+    return @config_hash[key]
+  end #def
+
+  def self.set(key, value)
+    @config_hash[key] = value
+    return @config_hash[key]
+  end #def
+
+  def self.save
+    File.open(@path, 'w') { |f| f.write(YAML.dump(@config_hash)) }
+    return @config_hash
   end #def
 end #class
